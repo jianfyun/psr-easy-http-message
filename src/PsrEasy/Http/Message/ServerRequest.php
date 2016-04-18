8a0ce85be3d156e1cd7404f32dad44fb0467f7e9
jianfyun <jianfyun@gmail.com>
2016-04-18 18:20:12 +0800

    revise server request

--- src/PsrEasy/Http/Message/ServerRequest.php
+++ src/PsrEasy/Http/Message/ServerRequest.php
@@ -54,6 +54,17 @@ class ServerRequest extends Request implements ServerRequestInterface
     protected $attributes = [];
 
     /**
+     * __construct 
+     * 
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->uri = new Uri($_SERVER['REQUEST_URI']);
+    }
+
+    /**
      * Retrieves all message header values.
      *
      * @access public
