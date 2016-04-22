37584c057a2c674d8a061457a3e529456341ceee
jianfyun <jianfyun@gmail.com>
2016-04-22 17:33:20 +0800

    revise ServerRequest.php

--- src/PsrEasy/Http/Message/ServerRequest.php
+++ src/PsrEasy/Http/Message/ServerRequest.php
@@ -64,7 +64,14 @@ class ServerRequest extends Request implements ServerRequestInterface
         if (isset($_SERVER['REQUEST_METHOD'])) {
             $this->withMethod($_SERVER['REQUEST_METHOD']);
             $this->withUri(new Uri($_SERVER['REQUEST_URI']));
+
             $this->parseHeaders();
+
+            $method = $this->getMethod();
+
+            if ($method == 'POST' || $method == 'PUT') {
+                $this->withBody(new Stream('php://input', 'r'));
+            }
         }
     }
 
